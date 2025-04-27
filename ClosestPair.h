09fcc86f8411040619664ecb24cc630ed33f0aae
Shreyansh Sharma <shreyanshsharma1010@gmail.com>
2025-04-27 03:34:00 +0000

    adding some useful comments

--- ClosestPair.h
+++ ClosestPair.h
@@ -7,37 +7,37 @@
 #include <chrono>
 #include "Point.h"
 
-class ClosestPair {
+class ClosestPair { // creating class
 public:
-    // Result structure to hold the pair of closest points and their distance
+    // Result structure to hold the pair of closest points and their distance 
     struct Result {
         int idx1;           // Index of first point
         int idx2;           // Index of second point
         double distance;    // Distance between points
         
-        Result() : idx1(-1), idx2(-1), distance(std::numeric_limits<double>::max()) {}
-        Result(int i, int j, double d) : idx1(i), idx2(j), distance(d) {}
+        Result() : idx1(-1), idx2(-1), distance(std::numeric_limits<double>::max()) {} // default constructor, sets the initial distance to max value of double data-type
+        Result(int i, int j, double d) : idx1(i), idx2(j), distance(d) {} //custom constructor
     };
     
-    // Brute force algorithm (ALG1)
-    static Result bruteForce(const std::vector<Point>& points);
+    // Brute force algorithm (ALG1) - function decleration, defination in ClosestPair.cpp file
+    static Result bruteForce(const std::vector<Point>& points); 
     
     // Divide and conquer algorithm (ALG2)
     static Result divideAndConquer(const std::vector<Point>& points);
     
 private:
-    // Helper functions for divide and conquer algorithm
-    static Result closestPairRec(const std::vector<Point>& Px, 
-                                const std::vector<Point>& Py);
+    // Helper functions for divide and conquer algorithm. This recursive function takes sorted list of points
+    static Result closestPairRec(const std::vector<Point>& Px, // takes points sorted by x-axis
+                                const std::vector<Point>& Py); // takes points sorted by y-axis
                                              
-    static Result stripClosest(const std::vector<Point>& strip, double d);
+    static Result stripClosest(const std::vector<Point>& strip, double d); // helper function to solve strip part
     
     // Comparator functions for sorting points
-    static bool compareX(const Point& a, const Point& b) {
+    static bool compareX(const Point& a, const Point& b) { //Sort points based on x-coordinates.
         return a.x < b.x;
     }
     
-    static bool compareY(const Point& a, const Point& b) {
+    static bool compareY(const Point& a, const Point& b) { //Sort points based on y-coordinates.
         return a.y < b.y;
     }
 };
